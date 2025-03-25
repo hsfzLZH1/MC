8ae29dd5a7c3b799e2962137d55ae3ad6fe8062f
hsfzLZH1 <hsfzlzh@outlook.com>
2025-03-26 00:32:28 +0800

    parse cmd args

--- LTL_MC/main.cpp
+++ LTL_MC/main.cpp
@@ -18,10 +18,23 @@ Closure C;
 GNBA GN;
 NBA N;
 
-int main(int argc, char * * argv) {
-  ts.ReadTS("TS.txt");
+int TSFile=-1,benchmarkFile=-1;
+bool isVerbose=false;
 
-  ifstream file("benchmark.txt",ios::in);
+int main(int argc, char * * argv) {
+  // parse command line arguments
+  for(int i=1;i<argc;i++)
+  {
+    if(strcmp(argv[i],"-v")==0)isVerbose=true;
+    else if(strcmp(argv[i],"-t")==0)TSFile=i+1,i++;
+    else if(strcmp(argv[i],"-b")==0)benchmarkFile=i+1,i++;
+    else{printf("Invalid command line argument.\n");return 0;}
+  }
+  
+  if(TSFile==-1)ts.ReadTS("TS.txt");
+  else ts.ReadTS(argv[TSFile]);
+
+  ifstream file((benchmarkFile==-1)?"benchmark.txt":argv[benchmarkFile],ios::in);
 
   int A,B,i;
   string str;
@@ -35,19 +48,23 @@ int main(int argc, char * * argv) {
     yy_switch_to_buffer(buf);
     yyparse();
     yy_delete_buffer(buf);
-    //print_expr(root);cout<<endl;//
+    if(isVerbose){cout<<"---\nPrint AST:\n";print_expr(root);cout<<endl;}//
     C.ComputeClosure(ts,root);
-    //C.PrintClosure();//
+    if(isVerbose){cout<<"---\nPrint Closure:\n";C.PrintClosure();}//
     GN.ComputeElem(C);
-    //GN.PrintElem();//
+    if(isVerbose){cout<<"---\nPrint Elementary Sets:\n";GN.PrintElem();}//
     GN.ComputeGF(C);
-    //GN.PrintGF();//
+    if(isVerbose){cout<<"---\nPrint GNBA:\n";GN.PrintGF();}//
     N.Construct(GN);
-    //N.Print();//
-    //ProdVerbose=true;//
+    if(isVerbose){cout<<"---\nPrint NBA:\n";N.Print();}//
+    if(isVerbose){cout<<"---\nPrint Product Construction:\n";ProdVerbose=true;}
     ProdConstruction(ts,N,prod);
     if(NestDFS(prod))printf("1\n");
-    else printf("0\n");//,PrintCounterEg();//
+    else
+    {
+      printf("0\n");
+      if(isVerbose){cout<<"---\nPrint Counter Example:\n";PrintCounterEg();}
+    }
   }
 
   while(B--)
@@ -61,18 +78,23 @@ int main(int argc, char * * argv) {
     yy_delete_buffer(buf);
     // change Initial set of TS to {i}
     ts.I.clear();ts.I.push_back(i);
-    //print_expr(root);cout<<endl;//
+    if(isVerbose){cout<<"---\nPrint AST:\n";print_expr(root);cout<<endl;}//
     C.ComputeClosure(ts,root);
-    //C.PrintClosure();//
+    if(isVerbose){cout<<"---\nPrint Closure:\n";C.PrintClosure();}//
     GN.ComputeElem(C);
-    //GN.PrintElem();//
+    if(isVerbose){cout<<"---\nPrint Elementary Sets:\n";GN.PrintElem();}//
     GN.ComputeGF(C);
-    //GN.PrintGF();
+    if(isVerbose){cout<<"---\nPrint GNBA:\n";GN.PrintGF();}//
     N.Construct(GN);
-    //N.Print();//
+    if(isVerbose){cout<<"---\nPrint NBA:\n";N.Print();}//
+    if(isVerbose){cout<<"---\nPrint Product Construction:\n";ProdVerbose=true;}
     ProdConstruction(ts,N,prod);
     if(NestDFS(prod))printf("1\n");
-    else printf("0\n");//,PrintCounterEg();
+    else
+    {
+      printf("0\n");
+      if(isVerbose){cout<<"---\nPrint Counter Example:\n";PrintCounterEg();}
+    }
   }
   return 0;
 }
